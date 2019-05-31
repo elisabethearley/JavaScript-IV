a1cebb7f1a321d66e1a4dc476a34a42d74524e00
elisabethearley <elisabethearley@gmail.com>
2019-05-30 23:30:21 -0400

    added things to log for each class to bottom of lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -22,7 +22,7 @@ class Instructor extends Person {
         console.log(`Today we are learning about ${subject}`);
     }
     grade(student, subject) {
-        console.log(`${student.name} receives a ${subject}`);
+        console.log(`${student.name} receives a perfect score on ${subject}!`);
     }
 }
 
@@ -34,14 +34,14 @@ class Student extends Person {
         this.favSubjects = studAttrs.favSubjects;
     }
     listsSubjects(favSubs) {
-       for( i = 0; i < favSubs.length; i++) {
+       for( let i = 0; i < favSubs.length; i++) {
         console.log(favSubs[i]);
        }
     }
-    PRAssignment(subject) {
-        console.log(`${student.name} has a submitted a PR for ${subject}`);
+    PRAssignment(student, subject) {
+        console.log(`${student.name} has submitted a PR for ${subject}`);
     }
-    sprintChallenge(subject) {
+    sprintChallenge(student, subject) {
         console.log(`${student.name} has begun sprint challenge on ${subject}`);
     }
 }
@@ -53,9 +53,95 @@ class ProjectManagers extends Instructor {
         this.favInstructor = pms.favInstructor;
     }
     standUp(channel) {
-        console.log(`${name} announces to ${channel}, @channel standby times!`);
+        console.log(`${this.name} announces to ${channel}, @channel standby times!`);
     }
     debugsCode(student, subject) {
-        console.log(`${name} debugs ${student.name}'s code on ${subject}`);
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
     }
 }
+
+const liz = new Person({
+    name: 'Liz',
+    age: 33,
+    location: 'Atlanta'
+})
+
+const david = new Person({
+    name: 'David',
+    age: 23,
+    location: 'Miami'
+})
+
+const marianne = new Instructor({
+    name: 'Marianne',
+    age: 45,
+    location: 'Seattle',
+    specialty: 'React',
+    favLanguage: 'JavaScript',
+    catchPhrase: "Don't let it get you down!"
+})
+
+const thomas = new Instructor({
+    name: 'Thomas',
+    age: 53, 
+    location: 'Washington, D.C.',
+    specialty: 'Node.js',
+    favLanguage: 'Javascript',
+    catchPhrase: 'Hang in there champ'
+})
+
+const rossen = new Student({
+    name: 'Rossen',
+    age: 23,
+    location: 'Birmingham',
+    previousBackground: 'Plumber',
+    className: 'WEBPT7',
+    favSubjects: ['HTML', 'CSS', 'JavaScript']
+})
+
+const calina = new Student({
+    name: 'Calina',
+    age: 31,
+    location: 'Detroit',
+    previousBackground: 'Ballet Dancer',
+    className: 'WEBPT6',
+    favSubjects: ['HTML', 'CSS', 'JavaScript']
+})
+
+const aaron = new ProjectManagers({
+    name: 'Aaron',
+    age: 43,
+    location: 'Wisconsin',
+    specialty: 'React',
+    favLanguage: 'JavaScript',
+    catchPhrase: "You got this!",
+    gradClassName: 'WEBPT1',
+    favInstructor: 'Dan'
+})
+
+const elisha = new ProjectManagers({
+    name: 'Elisha',
+    age: 31,
+    location: 'Savannah',
+    specialty: 'Node.js',
+    favLanguage: 'Javascript',
+    catchPhrase: 'Where you think you going? Get back here and learn!',
+    gradClassName: 'WEBPT2',
+    favInstructor: 'Josh'
+})
+
+liz.speak();
+david.speak();
+
+marianne.demo('React.');
+thomas.grade(rossen, 'Node.js');
+
+rossen.listsSubjects(rossen.favSubjects);
+rossen.PRAssignment(rossen, 'JavaScript.');
+calina.PRAssignment(calina, 'HTML.');
+calina.sprintChallenge(calina, 'JavaScript-IV.');
+
+aaron.standUp('Webpt7');
+elisha.debugsCode(calina, 'CSS');
+
+
